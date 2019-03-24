6306c7fd5afaea3d2fda1b78255e071371c12059
Aircity <aircity@drrr.us>
2019-03-24 17:13:47 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const Yaml = require('js-yaml')
+const fs = require('fs')
+
+function objectMap (object, mapFn) {
+  return Object.keys(object).reduce((result, key) => {
+    result[key] = mapFn(object[key])
+    return result
+  }, {})
+}
+
+module.exports = api => {
+  api.chainWebpack(config => {
+    const filename = api.resolve('env.yaml')
+    let env
+    if(fs.existsSync(filename)) {
+      env = Yaml.safeLoad(fs.readFileSync(filename, 'utf8'))
+    }
+    if (env && typeof env === 'object') {
+      const loadEnv = objectMap(env, value => JSON.stringify(value))
+      const getCurrentEnv = (args) => {
+        args['process.env'] = { ...args['process.env'], ...loadEnv }
+        return args
+      }
+      config.plugin('define').init((Plugin, args) => {
+        return new Plugin(getCurrentEnv(...args))
+      })
+    }
+  })
+}
